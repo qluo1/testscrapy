21d11408ecf6b111aa2cc1d881734faac3b873e9
luoqi <luoqi@yahoo.com>
2013-04-06 22:19:39 +1100

    working verison of webdriver

--- testscrapy/spiders/theage_spider.py
+++ testscrapy/spiders/theage_spider.py
@@ -1,4 +1,3 @@
-
 from scrapy.spider import BaseSpider
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
@@ -22,16 +21,32 @@ class TheAgeSpider(CrawlSpider):
 		print response.url
 		print hxs.select("//div[@class='articleBody']/p").extract()
 
-
-class TheAge_Spider(BaseSpider):
-	name = "basic_theage"
+import re
+import time
+from scrapy.http import Request
+from random import randint
+import codecs
+class YahooFinSpider(BaseSpider):
+	name = "yahoofin"
 	allowed_domains = ['au.finance.yahoo.com']
 	start_urls = ["http://au.finance.yahoo.com/news/topic-top-stories/"]
-	
+	p = re.compile("/news/.*\.html")
 	def parse(self,response):
 		print response.url
 		browser = webdriver.Remote("http://localhost:4445",{}) 
+		# browser = webdriver.Firefox() 
 		browser.get(response.url)
-		browser.get_screenshot_as_file("dump.png")
+		# for i in range(0,5):
+		# 	browser.find_element_by_xpath("//a[@class='more-link']").click()
+		# 	time.sleep(3)
 		hxs = HtmlXPathSelector(text=browser.page_source)
-		print hxs.select("//a/text()").extract()
+		links = hxs.select("//a/@href").extract()
+		browser.close()
+		return [Request("http://au.finance.yahoo.com"+i,callback=self.parse_article) for i in links if self.p.search(i)]
+
+	def parse_article(self,response):
+		print response.url
+		hxs = HtmlXPathSelector(response)
+		print hxs.select("//h1[@class='headline']/text()")
+		with codecs.open("%d.html" % randint(0,100),"w", encoding='utf-8') as f:
+			f.write(hxs.select("//div[@id='mediaarticlebody']").extract()[0])
