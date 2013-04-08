4aee03c551e03b7ba5e4673dc22b6e1a28e85cbf
luoqi <luoqi@yahoo.com>
2013-04-08 22:46:48 +1000

    fix searchable

--- searchable/yahoofin.py
+++ searchable/yahoofin.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from whoosh.index import create_in
+from whoosh.index import create_in, open_dir
 from whoosh.fields import  Schema, TEXT,ID
 from whoosh.qparser import QueryParser
 
@@ -10,14 +10,23 @@ schema = Schema(title=TEXT(stored=True), url=ID(stored=True), content=TEXT)
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def write_index(title,url,content):
-    ix = create_in(os.path.join(ROOT,"indexdir"), schema)
+    """ """
+    if not os.path.exists(os.path.join(ROOT,"indexdir")):
+        os.mkdir(os.path.join(ROOT,"indexdir"))
+        ix = create_in(os.path.join(ROOT,"indexdir"),schema)
+    else:
+        ix = open_dir(os.path.join(ROOT,"indexdir"))
+    
     writer = ix.writer()
-    writer.add_document(title=title, url=unicode(url),
-        content=content)
+    writer.add_document(title=title, url=unicode(url),content=content)
     writer.commit()
 
 def search_content(phase):
+    ix = open_dir(os.path.join(ROOT,"indexdir"))
+    ret = []
     with ix.searcher() as searcher:
-         query = QueryParser("content", ix.schema).parse(phase)
-         results = searcher.search(query)
-         return results
+        query = QueryParser("content", ix.schema).parse(phase)
+        results = searcher.search(query)
+        for i in results:
+            ret.append((i['title'],i['url']))
+        return ret
